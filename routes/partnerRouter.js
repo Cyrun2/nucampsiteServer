6d69f2369d92daefbb2f475ca1a0f3ef50183e8f
Cyrun <Cyrun2@gmail.com>
2021-11-13 11:49:34 -0500

    Workshop 2 done

---
+++ routes/partnerRouter.js
@@ -0,0 +1,74 @@
+const express = require('express');
+const Partner = require('../models/partners');
+
+const partnerRouter = express.Router();
+
+partnerRouter.route('/')
+.get((req, res, next) => {
+    Partner.find()
+    .then(partners => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(partners);
+    })
+    .catch(err => next(err));
+})
+.post((req, res, next) => {
+    Partner.create(req.body)
+    .then(partner => {
+        console.log('partner Created ', partner);
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(partner);
+    })
+    .catch(err => next(err));
+})
+.put((req, res) => {
+    res.statusCode = 403;
+    res.end('PUT operation not supported on /partners');
+})
+.delete((req, res, next) => {
+    Partner.deleteMany()
+    .then(response => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(response);
+    })
+    .catch(err => next(err));
+});
+
+partnerRouter.route('/:partnerId')
+.get((req, res, next) => {
+    Partner.findById(req.params.partnerId)
+    .then(repsonse => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(partner);
+    })
+})
+.post((req, res) => {
+    res.statusCode = 403;
+    res.end('PUT operation not supported on /partners/:partnerId');
+})
+.put((req, res, next) => {
+    Partner.findIdAndUpdate(req.params.partnerId, {
+        $set: req.body}, {new: true})
+    .then(partner => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(partner);
+    })
+    .catch(err => next(err));
+})
+.delete((req, res, next) => {
+    Partner.findIdAndDelete(req.params.partnerId)
+    .then(response => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(response);
+    })
+    .catch(err => next(err));
+});
+
+
+module.exports = partnerRouter;
