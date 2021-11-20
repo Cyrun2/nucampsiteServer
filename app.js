7af779d16acafe43ac692e30dace6051e181b6e8
Cyrun <Cyrun2@gmail.com>
2021-11-20 09:41:10 -0500

    added jwt and verifyUser

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
-const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const passport = require('passport');
 const authenticate = require('./authenticate');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+
+const config = require('./config');
 
 const app = express();
 
@@ -16,7 +17,7 @@ const promotionRouter = require('./routes/promotionRouter');
 const partnerRouter = require('./routes/partnerRouter');
 const mongoose = require('mongoose');
 
-const url = 'mongodb://localhost:27017/nucampsite';
+const url = config.mongoUrl;
 const connect = mongoose.connect(url, {
   useCreateIndex: true,
   useFindAndModify: false,
@@ -61,7 +62,7 @@ function auth(req, res, next) {
   }
 }
 
-app.use(auth);
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -70,7 +71,6 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser('12345-67890-09876-54321'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
